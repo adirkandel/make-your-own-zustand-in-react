e801a5d939d80bffd4dc9b63ebd1969f3a1ab8b0
adirkandel <adirkandel@gmail.com>
2025-11-01 19:59:10 +0200

    Refactor StoreTodoFavSelect and StoreTodoList components to use shallowEqual and deepEqual for optimized state comparisons, improving performance in the sync store demo.

--- src/pages/SyncStoreDemo/components/StoreTodoFavSelect.tsx
+++ src/pages/SyncStoreDemo/components/StoreTodoFavSelect.tsx
@@ -1,10 +1,10 @@
 import TodoFavSelect from "../../../components/TodoFavSelect";
 import { useStore, useTodoActions } from "../../../lib/sync-store";
-import { deepEqual } from "../../../lib/utils";
+import { shallowEqual } from "../../../lib/utils";
 
 const StoreTodoFavSelect = () => {
   const { markAsFavorite } = useTodoActions();
-  const todos = useStore(state => state.todos.map(todo => todo.text));
+  const todos = useStore(state => state.todos.map(todo => todo.text), shallowEqual);
   // const todosNames = useStore(
   //   state => state.todos.map(todo => ({ id: todo.id, text: todo.text })),
   //   deepEqual // NOT PERFORMANT
--- src/pages/SyncStoreDemo/components/StoreTodoList.tsx
+++ src/pages/SyncStoreDemo/components/StoreTodoList.tsx
@@ -2,9 +2,10 @@ import { memo } from "react";
 import StoreTodoItem from "./StoreTodoItem";
 import { useFilter, useStore } from "../../../lib/sync-store";
 import TodoList from "../../../components/TodoList";
+import { deepEqual } from "../../../lib/utils";
 
 const StoreTodoList = () => {
-  const todos = useStore(state => state.todos.sort(todo => (todo.isFavorite ? -1 : 1)));
+  const todos = useStore(state => state.todos.sort(todo => (todo.isFavorite ? -1 : 1)), deepEqual);
   const filter = useFilter();
 
   return (
